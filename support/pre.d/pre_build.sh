3cd5654d089e897ef5e36ed3da26b4dd03aa5ea9
Rafael Mori <faelmori@gmail.com>
2025-07-04 20:28:38 -0300

    Refactor pre_build.sh to encapsulate frontend build logic in a function
    - Moved frontend build steps into a dedicated function `build_frontend`.
    - Added silent flags to npm commands for cleaner output.
    - Improved error handling and messages for better clarity.
    - Ensured the script exits with appropriate messages on failure.
    
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- support/pre.d/pre_build.sh
+++ support/pre.d/pre_build.sh
@@ -8,31 +8,39 @@ set -o errtrace # If a command fails, the shell will exit immediately
 set -o functrace # If a function fails, the shell will exit immediately
 shopt -s inherit_errexit # Inherit the errexit option in functions
 
-_ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-if test -d "${_ROOT_DIR}/frontend"; then
-  cd "${_ROOT_DIR}/frontend" || exit 1
+build_frontend() {
+  _ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-  npm i -f || {
-    echo "❌ Falha ao instalar dependências do frontend. Verifique o log acima."
+
+  if test -d "${_ROOT_DIR}/frontend"; then
+    cd "${_ROOT_DIR}/frontend" || exit 1
+
+    npm i -f --silent || {
+      echo "❌ Falha ao instalar dependências do frontend. Verifique o log acima."
+      exit 1
+    }
+
+    npm run build --silent || {
+      echo "❌ Falha ao construir o frontend. Verifique o log acima."
+      exit 1
+    }
+  else
+    echo "❌ O diretório 'frontend' não foi encontrado em '${_ROOT_DIR}/frontend'."
     exit 1
-  }
+  fi
 
-  npm run build || {
-    echo "❌ Falha ao construir o frontend. Verifique o log acima."
+  _CP_EXEC=$(cp -r "${_ROOT_DIR}/frontend/build" "${_ROOT_DIR}/internal/services/server" 2>/dev/null || true)
+
+  if [[ ! -d "${_ROOT_DIR}/internal/services/server/build" ]]; then
+    echo "❌ O diretório de build não foi encontrado em '${_ROOT_DIR}/internal/services/server/build'."
     exit 1
-  }
-else
-  echo "❌ O diretório 'frontend' não foi encontrado em '${_ROOT_DIR}/frontend'."
+  else
+    echo "✅ Build copiado com sucesso para '${_ROOT_DIR}/internal/services/server/build'."
+  fi
+}
+
+build_frontend "$@" 2>/dev/null || {
+  echo "❌ Ocorreu um erro durante o processo de build do frontend. Verifique o log acima."
   exit 1
-fi
-
-_CP_EXEC=$(cp -r "${_ROOT_DIR}/frontend/build" "${_ROOT_DIR}/internal/services/server" 2>/dev/null || true)
-
-if [[ ! -d "${_ROOT_DIR}/internal/services/server/build" ]]; then
-  echo "❌ O diretório de build não foi encontrado em '${_ROOT_DIR}/internal/services/server/build'."
-  exit 1
-else
-  echo "✅ Build copiado com sucesso para '${_ROOT_DIR}/internal/services/server/build'."
-fi
-
+}
